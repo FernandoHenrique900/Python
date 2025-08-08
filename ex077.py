aab62eb9c077dfbbe697d175867f2fefff9b9bb1
Fernando Henrique <fernando.henrique14@outlook.com>
2025-08-08 19:25:53 -0300

    8-08-2025
    Organizando repositorio.

--- ex077.py
+++
@@ -1,9 +0,0 @@
-#Exercício Python 077: Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
-
-palavras=('APRENDER', 'PROGRAMAR', 'LINGUAGEM','PYTHON', 'CURSO', 'GRATIS', 'ESTUDAR','PRATICAR', 'TRABALHAR', 'MERCADO','PROGRAMADOR','FUTURO')
-
-for p in palavras: #FOR ANALISA CADA ELEMENTO DA TUPLA
-    print(f'\nNa palavra {p} temos', end=' ')
-    for letra in p: #ESSE FOR ANALISA AS VOGAIS
-        if letra in 'AaEeIiOoUu':
-            print(letra, end=' ')
